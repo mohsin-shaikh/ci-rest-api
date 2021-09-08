621891f3a0e12bd51db341ab285294b499943181
Mohsin Shaikh <mohsinshaikh78601@gmail.com>
2021-09-08 14:01:31 +0530

    Route Changes

--- app/Config/Development/Routes.php
+++ app/Config/Development/Routes.php
@@ -1,9 +1,13 @@
 <?php
 
-// $routes->resource('product');
+$routes->group('api/v1', ['namespace' => 'App\Controllers\API'], function ($routes) {
+  $routes->resource('product');
+  $routes->resource('client');
+});
 
-$routes->get('client', 'Client::index');
-$routes->post('client', 'Client::store');
-$routes->get('client/(:num)', 'Client::show/$1');
-$routes->post('client/(:num)', 'Client::update/$1');
-$routes->delete('client/(:num)', 'Client::destroy/$1');
+// Custom Routes for Client
+// $routes->get('client', 'Client::index');
+// $routes->post('client', 'Client::store');
+// $routes->get('client/(:num)', 'Client::show/$1');
+// $routes->post('client/(:num)', 'Client::update/$1');
+// $routes->delete('client/(:num)', 'Client::destroy/$1');
